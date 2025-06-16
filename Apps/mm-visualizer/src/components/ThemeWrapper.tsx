2a18295a853cbc167523a44c3ad493641f4136e2
Jin Le <jiqle@ucsc.edu>
2025-06-16 15:09:56 -0700

    Added session storage to theme

--- Apps/mm-visualizer/src/components/ThemeWrapper.tsx
+++ Apps/mm-visualizer/src/components/ThemeWrapper.tsx
@@ -2,24 +2,38 @@
 
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { lightTheme, darkTheme } from "../theme";
-import SunnyIcon from '@mui/icons-material/Sunny';
-import BedtimeIcon from '@mui/icons-material/Bedtime';
+import { lightTheme, darkTheme } from "../theme/theme";
+import SunnyIcon from "@mui/icons-material/Sunny";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
 import { Fab } from "@mui/material";
 import React from "react";
+import { storeCookieTheme } from "./actions";
 
 export default function ThemeWrapper({
   children,
+  initialTheme
 }: {
   children: React.ReactNode;
+  initialTheme: string
 }) {
-  const [light, setLight] = React.useState(true);
+  const [themeMode, setThemeMode] = React.useState(initialTheme);
+
+  const handleClick = async() => {
+    if (themeMode === 'light') {
+      setThemeMode('dark');
+      await storeCookieTheme('dark')
+    } else {
+      setThemeMode('light')
+      await storeCookieTheme('light')
+    }
+  };
   return (
-    <ThemeProvider theme={light ? lightTheme : darkTheme}>
+    <ThemeProvider theme={themeMode === 'dark' ? darkTheme : lightTheme}>
       <CssBaseline />
       {children}
       <Fab
-        onClick={() => setLight((prev) => !prev)}
+        aria-label="theme-button"
+        onClick={() => handleClick()}
         color="primary"
         sx={{
           position: "fixed",
@@ -28,7 +42,7 @@ export default function ThemeWrapper({
           zIndex: 9999,
         }}
       >
-        {light ? <BedtimeIcon/> : <SunnyIcon/>}
+        {themeMode === 'dark' ? <SunnyIcon /> : <BedtimeIcon />}
       </Fab>
     </ThemeProvider>
   );
